7081f6604676bb5af82487a04b7a2115393e8bef
Satchit-Sharma <148754957+Satchit-Sharma@users.noreply.github.com>
2023-12-01 12:52:58 +0530

    Update streamlit_app.py
    change 4 - added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('🥤 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚🍳 Hard-Boiled Free-Range Eggs')
 
 streamlit.header('Make your own Smoothie')
+
+import pandas as pd
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
