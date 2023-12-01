e785e3e09ee21401a176fcf2b4542119e1b7272e
Satchit-Sharma <148754957+Satchit-Sharma@users.noreply.github.com>
2023-12-01 13:12:08 +0530

    Update streamlit_app.py
    change 5 - adding multicheck

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,5 @@ streamlit.header('Make your own Smoothie')
 
 import pandas as pd
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
